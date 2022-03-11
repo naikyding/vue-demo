9cf95d839bda63ffde3e152df482af732f43f224
NaikyDing <74ding@gmail.com>
2022-03-11 15:48:34 +0800

    feat: setup composition api

--- vue2/vue2-pinia/src/views/About.vue
+++ vue2/vue2-pinia/src/views/About.vue
@@ -1,5 +1,18 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div>
+    {{title}}
   </div>
 </template>
+
+<script>
+export default {
+  setup () {
+    return {
+      title: 'Pinia'
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
