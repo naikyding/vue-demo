0e0a14a32256a617fec26711e32c0295945f881d
NaikyDing <74ding@gmail.com>
2022-03-14 16:38:05 +0800

    feat: getters demo

--- vue2/vue2-pinia/src/views/About.vue
+++ vue2/vue2-pinia/src/views/About.vue
@@ -10,6 +10,15 @@
     <input type="text" v-model="piniaStore.title">
 
     <p>這是解構獲取的值: {{ count }}</p>
+
+    <!-- getters -->
+    <div>
+      <p>{{ piniaStore.doubleCount }}</p>
+      <p>{{ piniaStore.doubleCountPlusOne }}</p>
+      <p>
+        {{ piniaStore.getUserById(3) }}
+      </p>
+    </div>
     </div>
   </div>
 </template>
@@ -25,6 +34,11 @@ export default {
 
     const piniaStore = useStore()
 
+    piniaStore.$subscribe((mutation, state) => {
+      console.log(mutation.type, mutation.storeId, mutation.payload)
+      console.log(mutation)
+    })
+
     // computed 獲取 state (響應)
     const count = computed(() => piniaStore.count)
 
@@ -36,13 +50,13 @@ export default {
     piniaStore.$patch({
       count: 222
     })
-    console.log(`第一種修改後 ${piniaStore.count}`)
+    console.log(`第二種修改後 ${piniaStore.count}`)
 
     setTimeout(() => {
       // 修改方式三 $patch (函式)
       piniaStore.$patch((state) => {
         state.count = 333
-        console.log(`第一種修改後 ${piniaStore.count}`)
+        console.log(`第三種修改後 ${piniaStore.count}`)
       })
     }, 3000)
 
