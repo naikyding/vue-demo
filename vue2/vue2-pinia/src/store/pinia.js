0fa60578ce8f813d6f4afb1bcbbc91d6f7da387e
NaikyDing <74ding@gmail.com>
2022-03-14 16:38:25 +0800

    feat: getters  settings

--- vue2/vue2-pinia/src/store/pinia.js
+++ vue2/vue2-pinia/src/store/pinia.js
@@ -3,6 +3,26 @@ import { defineStore } from 'pinia'
 export const useStore = defineStore('main', {
   state: () => ({
     title: 'pinia main',
-    count: 0
-  })
+    count: 0,
+    user: [
+      { id: 1, name: 'naiky', age: 34 },
+      { id: 2, name: 'nico', age: 32 },
+      { id: 3, name: 'niki', age: 7 }
+    ]
+  }),
+
+  getters: {
+    // 設置 getters
+    doubleCount: (state) => state.count * 2,
+
+    // 訪問其它 getters (不使用箭頭函式)
+    doubleCountPlusOne () {
+      return this.doubleCount + 1
+    },
+
+    // 傳遞參數給 getters
+    getUserById: (state) => {
+      return (userId) => state.user.find(user => user.id === userId)
+    }
+  }
 })
